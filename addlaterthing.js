0ab382f9629460e141f3448826d81a467abf4cf5
Stuart Gibson <stuart@abovetheinternet.org>
2014-12-17 11:44:20 +0000

    Quick and dirty commit

--- addlaterthing.js
+++ addlaterthing.js
@@ -1,9 +1,9 @@
 laterThingDiv='<div  id="laterthing-com-bookmarklet" style=" position:absolute;right:20px;top:20px;width:20em;height:5em;opacity:0.5;z-index:20001;              background:#000;text-align:center;border:solid 2px #ccc;"><p  id="laterthing-com-bookmarklet-p" style=" margin:0px;padding:0px;line-height:1em;              height:1em;position:absolute;top:50%;margin-top:-0.5em;width:100%;font-family: Helvetica, Arial, sans-serif;font-size: 20px;font-weight:lighter;">    Saving LaterLink…</p></div>';
 
 document.body.innerHTML+=laterThingDiv;
-userKey = document.getElementById("laterthing-com-user-key");
+userKey = document.getElementById("laterthing-com-user-key").innerHTML;
 console.log(userKey);
-var request=new XMLHttpRequest;request.open("POST","http://laterthing.com/later-links/bookmarklet.json?link_url="+encodeURIComponent(location.href)+"&user_key="+userKey+"&page_title="+encodeURIComponent(document.title),true);
+var request=new XMLHttpRequest;request.open("POST","http://laterthing.com/later-links/bookmarklet.json?link_url="+encodeURIComponent(location.href)+"&user_key="+encodeURIComponent(userKey)+"&page_title="+encodeURIComponent(document.title),true);
 
 request.onreadystatechange=function()
 {
