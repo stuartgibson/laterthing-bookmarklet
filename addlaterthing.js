ed747dc8beea62dbbf115bb12fce7bb332a68581
Stuart Gibson <stuart@abovetheinternet.org>
2014-12-17 10:50:45 +0000

    wrap hmtl

--- addlaterthing.js
+++ addlaterthing.js
@@ -1,30 +1,4 @@
-laterThingDiv='
-<div  id="laterthing-com-bookmarklet"
-      style=" position:absolute;
-              right:20px;
-              top:20px;
-              width:20em;
-              height:5em;
-              opacity:0.5;
-              z-index:100;
-              background:#000;
-              text-align:center;
-              border:solid 2px #ccc;">
-  <p  id="laterthing-com-bookmarklet-p"
-      style=" margin:0px;
-              padding:0px;
-              line-height:1em;
-              height:1em;
-              position:absolute;
-              top:50%;
-              margin-top:-0.5em;
-              width:100%;
-              font-family: Helvetica, Arial, sans-serif;
-              font-size: 20px;
-              font-weight:lighter;">
-    Saving LaterLink…
-  </p>
-</div>';
+laterThingDiv='<div  id="laterthing-com-bookmarklet" style=" position:absolute;right:20px;top:20px;width:20em;height:5em;opacity:0.5;z-index:20001;              background:#000;text-align:center;border:solid 2px #ccc;"><p  id="laterthing-com-bookmarklet-p" style=" margin:0px;padding:0px;line-height:1em;              height:1em;position:absolute;top:50%;margin-top:-0.5em;width:100%;font-family: Helvetica, Arial, sans-serif;font-size: 20px;font-weight:lighter;">    Saving LaterLink…</p></div>';
 
 document.body.innerHTML+=laterThingDiv;
 var request=new XMLHttpRequest;request.open("POST","http://localhost:5000/later-links/bookmarklet.json?link_url="+encodeURIComponent(location.href)+"&user_key=d39e7d47-2949-4254-a29e-9648b36c3287&page_title="+encodeURIComponent(document.title),true);
